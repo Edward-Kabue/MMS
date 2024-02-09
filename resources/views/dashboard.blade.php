e76b8321caa44658fd809b4b6f7b34cadf996aae
Edward-Kabue <ekabue0@gmail.com>
2024-02-09 17:18:07 +0300

    Add a home component and welcome page ui

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,17 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            {{ __('My tasks') }}
         </h2>
     </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    <!--show all posts created by the user
+    @forelse ($tasks as $task)
+        <div class="py-4">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900">
+                        <!--show all posts created by the user
                     if not posts display no posts-->
-                    @forelse ($tasks as $task)
+
                         <div class="mb-4">
                             <h2 class="text-2xl font-bold">{{ $task->name }}</h2>
                             <p class="text-gray-600">{{ $task->description }}</p>
@@ -20,11 +20,12 @@
                             </div>
                         </div>
 
-                    @empty
-                        <p>No tasks</p>
-                    @endforelse
+
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+    @empty
+        <p>No tasks</p>
+    @endforelse
 </x-app-layout>
