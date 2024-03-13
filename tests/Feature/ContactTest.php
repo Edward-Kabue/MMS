e290515d87150cefc93c4a1ba0f593bba594347a
Edward-Kabue <ekabue0@gmail.com>
2024-03-13 17:28:53 +0300

    Add user creation and role assignment in ContactTest

--- tests/Feature/ContactTest.php
+++ tests/Feature/ContactTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use App\Models\User;
 use App\Models\Contact;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
@@ -13,7 +14,15 @@ class ContactTest extends TestCase
  use RefreshDatabase;
    
     public function test_can_create_contact(): void {
-        $user = User::factory()->create();
+        $user = User::create([
+            'name' => 'super admin',
+            'is_admin' => 1,
+            'email' => 'test@admin.com',
+            'password' => Hash::make('password'),
+        ]);
+        //assign the user a role of admin
+        $this->artisan( 'db:seed' );
+        $user->assignRole('super-admin');
         $this->actingAs($user);
         $response = $this->get('/admin/contacts/create');
         $response->assertStatus(200);
