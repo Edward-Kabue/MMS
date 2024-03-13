b28df509aeda85785341051f49ed84ab6182eafc
Edward-Kabue <ekabue0@gmail.com>
2024-03-13 18:01:13 +0300

    Add OrganizationPolicy and update RolesAndPermissionsSeeder and CreateOrgaizationTest

--- tests/Feature/CreateOrgaizationTest.php
+++ tests/Feature/CreateOrgaizationTest.php
@@ -14,21 +14,24 @@ class CreateOrgaizationTest extends TestCase
     use RefreshDatabase;
     use HandlesAuthorization;
    
-    //Fetch the user with super admin role and create an organization
-    public function test_can_create_organization(): void {
+  
+    
+    public function test_super_admin_can_create_organization(): void {
         $user = User::factory()->create();
+        $this->artisan( 'db:seed' );
+        $user->assignRole( 'super-admin' );
         $this->actingAs($user);
         $response = $this->get('/admin/organizations/create');
         $response->assertStatus(200);
     }
-    
-    public function test_can_update_organization(): void {
+    //asset that the user with the role of sales cannot delete an organization
+    public function test_sales_person_cannot_edit_organization(): void {
         $user = User::factory()->create();
-        //create an organization factory
-        Organization::factory()->create(['name'=> 'Organization 1']);
+        $this->artisan( 'db:seed' );
+        $user->assignRole( 'sales' );
         $this->actingAs($user);
+        Organization::factory()->create(['id' => 1]);
         $response = $this->get('/admin/organizations/1/edit');
-        $response->assertStatus(200);
+        $response->assertStatus(403);
     }
-    
 }
